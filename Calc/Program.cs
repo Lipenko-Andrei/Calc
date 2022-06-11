1c0c77e71d0c4fd564049f73a17e4a269dfc68ad
Lipenko-Andrei <Lipenko3005andrei@gmail.com>
2022-06-11 13:00:31 +0400

    Добавьте файлы проекта.

---
+++ Calc/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Console.WriteLine("2 кг\n13 17 ");
+        }
+    }
+}
